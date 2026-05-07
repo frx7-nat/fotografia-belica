5dd318d8e9e2ad420015a00bd1fb90a5374cfacd
natalier <natalier.profissional@gmail.com>
2026-05-07 15:26:57 -0300

    Adiciona slugs aos posts

--- posts.js
+++ posts.js
@@ -1,6 +1,7 @@
 const posts = [
   {
     id: 1,
+    slug: "fotografia-e-aparencia",
     title: "A fotografia é aparência?",
     date: "2026-04-01",
     content: "Não, a fotografia é labor. Retratar, seja o que for, vai requerer um ímpeto que fachada alguma resistiria sem alicerce. O visual facilmente dura o tempo de uma impressão, o trabalho pode durar o tempo de uma vida."
