8a8e9eead5bae8ef7eb7df7dd14d692cd825e274
natalier <natalier.profissional@gmail.com>
2026-05-12 09:09:07 -0300

    Novo post: Deixei o Direito pela fotografia

--- posts.js
+++ posts.js
@@ -1,4 +1,27 @@
 const posts = [
+{
+  id: 2,
+  slug: "deixei-o-direito-pela-fotografia",
+  title: "Deixei o direito pela fotografia",
+  date: "2026-05-11",
+   content: `Não foi uma decisão fácil, nunca é, mas em algum momento precisei criar minha resposta.
+
+Se valeu a pena, eis uma resposta simples, com absoluta certeza, facilmente o rigor técnico da lei teria engolido meus pueris sonhos.
+
+Não que aqueles 5 anos nada tenham representado, todo um estudo sobre o pacto social e ordenamento jurídico me ajudaram a criar uma base inclusive para ver a fotografia com mais seriedade, já que o olhar e registro cotidiano também permeiam tão profundo nossa sociedade.
+
+Hoje sou fotógrafo e tenho 28 anos, muito novo inclusive para contar história, mas convicto e apaixonado o suficiente para falar que as possibilidades são infinitamente maiores onde bate o coração.
+
+Quando assumi de fato a fotografia, vi que ali era apenas mais uma porta para outra infinidade de conhecimentos. Comunicação, marketing, escrita, tudo que significa trânsito de ideias poderia auxiliar meu visual.
+
+Com o tempo o caminho fica menos turvo e o dia amanhece, a fotografia vira uma associação instantânea e um laborioso laboratório.
+
+Olhando em retrospecto, os últimos 5 anos foram valorosos, basicamente o tempo de outra faculdade, mas que foi estudada em campo, sem professor e sem manual.
+
+Por fim, há um ditado que diz o direito não socorre aos que dormem.
+
+Nem o direito, nem a vida, e nessa história eu quis ficar acordado.`
+  },
   {
     id: 1,
     slug: "fotografia-e-aparencia",
